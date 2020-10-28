39026c46857846930d440b0addaf3f4b4d07e3ef
Alan Liddell <alan.c.liddell@gmail.com>
2020-10-27 21:22:48 -0400

    update dependencies

--- deps/build.jl
+++ deps/build.jl
@@ -7,8 +7,11 @@ import Fluster.Constants: FLUSTER_ENV
 Conda.pip_interop(true, FLUSTER_ENV)
 
 # add required Python packages
+pipdeps = [
+    "https://github.com/SpikeInterface/spikeextractors/archive/1845ebae164e82e8b35974f3fc7665e238ce99ad.tar.gz#egg=spikeextractors"
+]
 Conda.add("pynwb", FLUSTER_ENV; channel="conda-forge")
-Conda.pip("install", ["spikeinterface"], FLUSTER_ENV)
+Conda.pip("install", pipdeps, FLUSTER_ENV)
 
 # set PyCall's python to our conda env and rebuild
 ENV["PYTHON"] = joinpath(Conda.python_dir(FLUSTER_ENV), "python")
