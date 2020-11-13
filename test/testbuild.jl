19fdf63c4de46cc0505591d4548e89ac68e20d48
Alan Liddell <alan.c.liddell@gmail.com>
2020-11-13 10:29:11 -0500

    build package pre-test

--- test/testbuild.jl
+++ test/testbuild.jl
@@ -2,6 +2,9 @@ import Conda
 import PyCall
 import Fluster.Constants: FLUSTER_ENV
 
+using Pkg
+Pkg.build()
+
 function syspy()
     "python" * (Base.Sys.iswindows() ? ".exe" : "")
 end
@@ -11,6 +14,6 @@ end
 end
 
 @testset "Imports OK" begin
-    @test PyCall.pyimport("spikeinterface") isa PyCall.PyObject
+    @test PyCall.pyimport("spikeextractors") isa PyCall.PyObject
     @test PyCall.pyimport("pynwb") isa PyCall.PyObject
 end
